b90647101dd8f65ded8d902385a74d4de0ce0754
ffelipedev <69429894+ffelipedev@users.noreply.github.com>
2021-01-08 10:54:29 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       getPages: [
-        //name = rota
+         
         GetPage(name: '/', page: () => HomePage()),
       ],
     );
